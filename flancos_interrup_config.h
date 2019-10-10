23c09981552891728b9eee5186627596c009532e
alvarobrun <54553974+alvarobrun@users.noreply.github.com>
2019-10-10 19:36:48 -0300

    Update flancos_interrup_config.h

--- flancos_interrup_config.h
+++ flancos_interrup_config.h
@@ -1,3 +1,5 @@
+// Configuracion de interrupciones para que se interrumpa con flancos de subida, de bajadas o con ambos 
+
 #ifndef Flancos_interrup_config
 #define Flancos_interrup_config
 
@@ -5,36 +7,37 @@
 //----------- Boton A ----------------
 
 #define BTNA_SetIntLowEdge() do {\
-CNEN0A = 0;\
-CNEN1A = 1;\
+    CNEN0Abits.CNIE0A13=0;\
+    CNEN1Abits.CNIE1A13=1;\
 } while(0);
 
+     
 #define BTNA_SetIntHighEdge() do {\
-CNEN0A = 1;\
-CNEN1A =0;\
+    CNEN0Abits.CNIE0A13=1;\
+    CNEN1Abits.CNIE1A13=0;\
   } while(0);
 
 #define BTNA_SetIntBothEdge() do {\
-CNEN0A = 1;\
-CNEN1A =1;\
+    CNEN0Abits.CNIE0A13=1;\
+    CNEN1Abits.CNIE1A13=1;\
   } while(0);
 
 
 //------------- Boton B ------------------
 
 #define BTNB_SetIntLowEdge() do {\
-CNEN0B = 0;\
-CNEN1B = 1;\
+    CNEN0Bbits.CNIE0B15=0;\
+    CNEN1Bbits.CNIE1B15=1;\
 } while(0);
 
 #define BTNB_SetIntHighEdge() do {\
-CNEN0B = 1;\
-CNEN1B =0;\
-  } while(0);
+    CNEN0Bbits.CNIE0B15=1;\
+    CNEN1Bbits.CNIE1B15=0;\
+ }while(0);
 
 #define BTNB_SetIntBothEdge() do {\
-CNEN0B = 1;\
-CNEN1B =1;\
+    CNEN0Bbits.CNIE0B15=1;\
+    CNEN1Bbits.CNIE1B15=1;\
   } while(0);
 
 
