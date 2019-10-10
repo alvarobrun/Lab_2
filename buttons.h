4444a978d763459edcc27d0554524737ba2e8a4c
alvarobrun <54553974+alvarobrun@users.noreply.github.com>
2019-10-10 19:34:16 -0300

    Update buttons.h

--- buttons.h
+++ buttons.h
@@ -1,4 +1,4 @@
-#include <xc.h> //NO SE LA DIFERENCIA CON EL DE ABAJO
-//#include "xc-pic32m.h"
-void __ISR(_CHANGE_NOTICE_B_VECTOR , IPL2SOFT) _CHANGE_NOTICE_B(void);
- 
+#include "HardwareProfile.h"
+#include <xc.h> // libreria que tiene la funcion IFSOCLR y tmb esta definido ahi el _CHANGE_NOTICE_...
+#include <sys/attribs.h> // libreria que tiene la funcion __ISR
+#include <proc/p32mm0256gpm064.h>
